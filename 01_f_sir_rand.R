90d019c96638e071e4f56dd60aeda48d17cdab05
junyitt <junyitt1995@gmail.com>
2017-04-29 15:43:29 +0800

    3: working for 2 places

--- 01_f_sir_rand.R
+++ 01_f_sir_rand.R
@@ -1,22 +1,63 @@
+lambda.f <-function(nPlace, time){
+      runif(nPlace^2, 10,100)*exp(-0.05*time);
+}
 
+generateM.f <- function(nPlace, seed, time){
+      set.seed(seed)
+      k <- rpois(n = nPlace^2, lambda = lambda.f(nPlace, time))
+      mMat <- matrix(k, nrow = nPlace, ncol = nPlace, byrow = T)
+      diag(mMat) <- 0
+      return(mMat)
+}
+
+# rk4 <- function(G,t, tstep, M){
+#       s <- G[1];
+#       i <- G[2];
+#       r <- G[3];
+      
+rk4 <- function(G, tstep, M){
+      t <- G[1];
+      s <- G[2];
+      i <- G[3];
+      r <- G[4];
+      
+      h = tstep/2.0;
+      
+      s1 = tstep * derivS(t, s, i, r);
+      i1 = tstep * derivI(t, s, i, r, M);
+      r1 = tstep * derivR(t, s, i, r);
+      
+      s2 = tstep * derivS(t+h, s+s1/2.0, i+i1/2.0, r+r1/2.0);
+      i2 = tstep * derivI(t+h, s+s1/2.0, i+i1/2.0, r+r1/2.0, M);
+      r2 = tstep * derivR(t+h, s+s1/2.0, i+i1/2.0, r+r1/2.0);
+      
+      s3 = tstep * derivS(t+h, s+s2/2.0, i+i2/2.0, r+r2/2.0);
+      i3 = tstep * derivI(t+h, s+s2/2.0, i+i2/2.0, r+r2/2.0, M);
+      r3 = tstep * derivR(t+h, s+s2/2.0, i+i2/2.0, r+r2/2.0);
+      
+      s4 = tstep * derivS(t+tstep, s+s3, i+i3, r+r3);
+      i4 = tstep * derivI(t+tstep, s+s3, i+i3, r+r3, M);
+      r4 = tstep * derivR(t+tstep, s+s3, i+i3, r+r3);
+      
+      s = s + (s1 + (2.0*(s2 + s3)) + s4)/6.0;
+      i = i + (i1 + (2.0*(i2 + i3)) + i4)/6.0;
+      r = r + (r1 + (2.0*(r2 + r3)) + r4)/6.0;
+      
+  return(c(t,s,i,r));
+      
+}
 
 
 derivS <- function( tdummy,  sdummy,  idummy,  rdummy){
       
-      return(-1.0*beta*sdummy*idummy ); 
+      return(-1.0*betap*sdummy*idummy ); 
       
 }
 derivI <- function( tdummy,  sdummy,  idummy,  rdummy, M.mat){
-      return(beta*sdummy*idummy - gamma*idummy - M.mat[0,1] + M.mat[1,0]); #add random exp
+      return(betap*sdummy*idummy - gammap*idummy - M.mat[1,2] + M.mat[2,1]); #add random exp
 }
 
 derivR <- function( tdummy, sdummy,  idummy,  rdummy){
-      return(gamma*idummy);
+      return(gammap*idummy);
 }
 
-Mrand <- function( seed,  lambda.v, t){
-      
-      for(int j = 0; j < nPlace; j++){
-            M[j] = rpois(lambda[j]*exp(-0.05*t), seed);
-      }
-}
--- 01_sirRand.R
+++ 01_sirRand.R
@@ -1,126 +1,55 @@
-#include<stdio.h>
-#include<iostream>
-#include<fstream>
-#include<conio.h>
-#include <new>
+library(plyr);library(dplyr);
+library(reshape2)
+library(ggplot2)
+endtime <- 120; 
+tstep <- 0.05;
+nsteps = endtime/tstep;
 
-// double beta = 0.001;
-// double gamma = 0.1; 
-// double tstep = 0.1;
-// double nsteps = 1000;
-double tstep = 0.05;
-double nsteps = 4000;
+pp = c(1e6, 1e6);
+ss = pp*0.999;
+ii = pp*0.001;
+rr = c(0,0);
+      bbeta = 0.95/pp
+      ggamma = 0.33333; 
+t = 0;
+nPlace <- 2;
 
-double beta = 0.000095;
-double gamma = 0.33333; 
-double ss = 10000;
-double ii = 10;
-double rr = 0;
-double tt = 0;
+tInt <-seq(from=0, to=endtime, by = tstep)
 
-//MRand
-int nPlace = 2;		
-double* M = new double[nPlace];
+gdf0 <- data.frame(t = 0, s = ss, i = ii, r = rr)
+
+df_t <<- gdf0
 
 
 
-void rk4(double *ss, double *ii, double *rr, double *tt, double M[]);
-double derivS(double tdummy, double sdummy, double idummy, double rdummy);
-double derivI(double tdummy, double sdummy, double idummy, double rdummy, double M[]);
-double derivR(double tdummy, double sdummy, double idummy, double rdummy);
-int rpois(double lambda, int seed);
-void Mrand(double seed, double lambda[], double t);
-
-using namespace std;
-
-
-void main(){
-      int j;
-      
-      //write file
-      ofstream myfile ("data_sir_rand.csv");
-      if (myfile.is_open())
-      {
-            //header
-            myfile << "time, S, I, R, M0, M1\n";
+a <-lapply(1:nPlace, FUN = function(row){
+#track migration
+dfmat <<- data.frame()
+cname.dfmat <- paste0("M", row, 1:nPlace);
+      # g <- as.numeric(df_t[row,2:4])
+      g <- as.numeric(df_t[row,1:4])
+      betap <<- bbeta[row]
+      gammap <<- ggamma[row]
+      sir_list <- lapply(tInt, FUN = function(x){
+            g[1] <- g[1] + tstep
+            seed <- ceiling(runif(1,1,100000));
+            mMat <- generateM.f(nPlace = 2, seed = seed, g[1])
+                  dfmat <<- rbind(dfmat, mMat[row,])
+            # g <<- rk4(g[2:4],x, tstep, mMat);
             
-            //Loop #####
-            //call rk4 function
-            for(j = 1; j <= nsteps; j++){
-                  tt = j*tstep;
-                  
-                  //mrand
-                  double seed = rand();
-                  
-                  double *lambdaPlace = new (nothrow) double[nPlace];	
-                  //initialize lambdal
-                  for(int k = 0; k < nPlace; k++){
-                        lambdaPlace[k] = 20-k; 
-                  }
-                  Mrand(seed, lambdaPlace, tt);
-                  
-                  //call rk4
-                  rk4(&ss, &ii, &rr, &tt, M);
-                  //write
-                  myfile << tt <<"," << ss << "," << ii << "," << rr << "," << M[0] << "," << M[1] << "\n";
-            }
-            
-            //done
-            myfile.close();
-            cout << "Output csv completed..." << endl;
-            
-      }
-      else
-      {	  
-            cout << "Unable to open file";
-      }
-      _getch();
-      
-}
+            g <<- rk4(g, tstep, mMat);
+            return(g)
+      })
+      df <- as.data.frame(do.call(rbind, sir_list))
+      colnames(df) <- c("time", "s", "i","r")
+      colnames(dfmat) <- cname.dfmat
+      #combine
+      df2 <- data.frame(df, dfmat)
+      return(df2)
+})
 
-void rk4(double *ss, double *ii, double *rr, double *tt, double M[]){
-      
-      double s, i, r, t;
-      double h;
-      double s1, s2, s3, s4;
-      double i1, i2, i3, i4;
-      double r1, r2, r3, r4;
-      
-      
-      s = *ss;
-      i = *ii;
-      r = *rr;
-      t = *tt;
-      
-      h = tstep/2.0;
-      
-      s1 = tstep * derivS(t, s, i, r);
-      i1 = tstep * derivI(t, s, i, r, M);
-      r1 = tstep * derivR(t, s, i, r);
-      
-      s2 = tstep * derivS(t+h, s+s1/2.0, i+i1/2.0, r+r1/2.0);
-      i2 = tstep * derivI(t+h, s+s1/2.0, i+i1/2.0, r+r1/2.0, M);
-      r2 = tstep * derivR(t+h, s+s1/2.0, i+i1/2.0, r+r1/2.0);
-      
-      s3 = tstep * derivS(t+h, s+s2/2.0, i+i2/2.0, r+r2/2.0);
-      i3 = tstep * derivI(t+h, s+s2/2.0, i+i2/2.0, r+r2/2.0, M);
-      r3 = tstep * derivR(t+h, s+s2/2.0, i+i2/2.0, r+r2/2.0);
-      
-      s4 = tstep * derivS(t+tstep, s+s3, i+i3, r+r3);
-      i4 = tstep * derivI(t+tstep, s+s3, i+i3, r+r3, M);
-      r4 = tstep * derivR(t+tstep, s+s3, i+i3, r+r3);
-      
-      s = s + (s1 + (2.0*(s2 + s3)) + s4)/6.0;
-      i = i + (i1 + (2.0*(i2 + i3)) + i4)/6.0;
-      r = r + (r1 + (2.0*(r2 + r3)) + r4)/6.0;
-      
-      *ss = s;
-      *ii = i;
-      *rr = r;
-      *tt = t;
-      
-}
-
-
+df <- a[[1]]
+df2 <- melt(df, id.vars = "time")
+ggplot(df2, aes(x=time, y = value, group = variable, color = variable)) + geom_point()
 
 
